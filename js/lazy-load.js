ac4162acd2e9da67f9d80e792939c5bc2f92f7f2
Ariel Güelfi <arguelfi@gmail.com>
2016-12-23 17:15:36 -0300

    Play around with Materialize css

--- js/lazy-load.js
+++ js/lazy-load.js
@@ -12,12 +12,12 @@ window.onload = function () {
     win = window;
     doc = win.document;
 
-    var bigSrc = (function () {
+    var bigSrc = function (selector) {
         if (doc.styleSheets) {
             var cssRules = doc.styleSheets[1].cssRules, fullImg;
             for (var i = 0; i < cssRules.length; i++) {
                 var rule = cssRules[i];
-                if (rule.selectorText == ".placeholder-full") {
+                if (rule.selectorText == selector) {
                     fullImg = rule;
                     break;
                 }
@@ -26,26 +26,45 @@ window.onload = function () {
                 return fullImg.style.backgroundImage.slice(4, -1).replace(/"/g, "");
             }
         }
-    }());
-
-//        var placeholder = document.querySelector('.placeholder'),
-//            small = placeholder.querySelector('.img-small');
-    var placeholder = doc.querySelector('.placeholder');
+    };
 
     // 1: load small image and show it
+    var small = doc.querySelector('.img-small'), parallax = doc.querySelector('.parallax'),
+        placeholder = doc.querySelector('.placeholder');
     var img = new Image();
     img.onload = function () {
-        placeholder.classList.add('loaded');
+        placeholder.appendChild(img);
+        placeholder.classList.add('img-small', 'loaded');
     };
-    img.src = getImgSrc(placeholder);
+    img.src = bigSrc(".img-small");
 
-    if (bigSrc) {
-        // 2: load large image
-        var imgLarge = new Image();
-        imgLarge.onload = function () {
-            placeholder.classList.add('placeholder-full');
-            placeholder.classList.remove('img-small');
-        };
-        imgLarge.src = bigSrc;
-    }
+    // 2: load large image
+    var imgLarge = new Image();
+    imgLarge.onload = function () {
+        img.src = imgLarge.src;
+        placeholder.classList.remove('img-small');
+    };
+    imgLarge.src = bigSrc('.img-full');
+    // var placeholder = doc.querySelector('.placeholder');
+    //
+    // // 1: load small image and show it
+    // var img = new Image();
+    // img.onload = function () {
+    //     placeholder.classList.add('loaded');
+    // };
+    // img.src = getImgSrc(placeholder);
+    //
+    // if (bigSrc) {
+    //     // 2: load large image
+    //     var imgLarge = new Image();
+    //     imgLarge.onload = function () {
+    //         placeholder.classList.add('placeholder-full');
+    //         placeholder.classList.remove('img-small');
+    //     };
+    //     imgLarge.src = bigSrc;
+    // }
+    $(document).ready(function () {
+        $('.parallax').parallax();
+        $('.scrollspy').scrollSpy();
+    });
 };
